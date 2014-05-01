dfe0beb35b577f817e8bd017985bc6f90e61397c
Wender <wender.lima@gmail.com>
2014-05-01 20:42:55 -0300

    Adding initial files

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-angular-cardflag
-================
+# Card Flag directive for [AngularJS](http://angularjs.org/)
+
+***
 
 AngularJS directive to automatically show credit card flags while typing
+
+
+This directive will look for attribute angular-cardflat on input field, will add an class card-flag and an SPAN after it with a class _flag,
+when you start typing a number, it will add a class corresponding to the Credit Card brand.
+
+All the magic happens on your CSS
+
+## Example: 
+```HTML
+<input name="test" maxlengh="19" angular-cardflag />
+```
+
+
+## Demo
+Do you want to see this directive in action? Visit http://wender.com.br/angular/angular-cardflag.html
---
+++ src/angular-cardflag.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('angularCardflag',[])
+    .directive('angularCardflag', function() {
+      return {
+        restrict: 'A',
+        link: function($scope, el) {
+            el.addClass('card-flag');
+            el[0].insertAdjacentHTML('afterend','<span class="_flag"></span>');
+            function flag(el) {
+                var c = {
+                    'amex' : [34,37],
+                    'visa' : [4],
+                    'master' : [51,52,53,54,55],
+                    'diners' : [300,301,302,303,304,305,2014,2149,36],
+                    'hipercard' : [38],
+                    'aura' : [50],
+                    'elo' : [63]
+                };
+                var matched = false;
+                for(var a in c){
+                    if(!matched)
+                        for(var b in c[a]){
+                            var r = new RegExp("^"+c[a][b]);
+                            var remove = el[0].className.match(/\b(card-flag)\.?[^\s]+/g);
+                            var css = el[0].className;
+                            if(el[0].value.match(r)){
+                                matched = true;
+                                el[0].className = css.replace(remove,'').concat(' card-flag-'+a);
+                                break;
+                            }else{
+                                el[0].className = css.replace(remove,'');
+                            }
+                        }
+                    }
+                }
+
+                el.bind('keyup keydown blur', function(e) {
+                    flag(el);
+                });
+            }
+        };
+});
+
